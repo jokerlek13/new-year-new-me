8283193eee7aebb1b571001f566ddb5d550b17e5
jokerlek13 <1LECAI82@solent.ac.uk>
2023-12-16 10:33:06 +0000

    ma

---
+++ week 8/plots.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def small():
+    x = [3, 10, 10, 3]
+    y = [3, 3, 10, 10]
+    plt.plot(x,y)
+    plt.show
+
+def medium():
+    pass
+
+
+def large():
+    pass

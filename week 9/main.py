dc1317837d623661163137f77dee53a987186c7c
jokerlek13 <1LECAI82@solent.ac.uk>
2024-01-06 11:23:30 +0000

    ma

--- week 9/main.py
+++ week 9/main.py
@@ -1,9 +1,13 @@
 from Human import Human
+from Robot import Robot
 
 
 
 if (__name__ == '__main__'):
-
-
+    print(Human.MAX_ENERGY)
     beautiful_human = Human()
     beautiful_human.display()
+
+
+
+
